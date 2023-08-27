a8546567ec6b1459d8e68ae1acc6f659dedc4e3e
Queen <queennefertitishabazz@gmail.com>
2023-08-27 13:02:31 -0700

    Update sendEmail.ts

--- pages/api/sendEmail.ts
+++ pages/api/sendEmail.ts
@@ -9,8 +9,8 @@ export default async function SendEmail(req: NextApiRequest, res: NextApiRespons
   const referer = req.headers.referer;
 
   const content = {
-    to: ['queenscriptqueen@gmail.com'],
-    from: 'queenscriptqueen@gmail.com',
+    to: ['queenscriptqueen@gmail.com','tekiaanderson5@gmail.com','mrsceo44@gmail.com'],
+    from: 'contact@newlifescs.com',
     subject: subject,
     text: description,
     html: `<div>

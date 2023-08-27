37e12cc2e186ba501c8d7b50bb8387f56d28ebb3
Queen <queennefertitishabazz@gmail.com>
2023-08-27 14:38:21 -0700

    Update env.ts

--- env.ts
+++ env.ts
@@ -2,5 +2,5 @@ export const EnvVars = {
   SITE_NAME: 'New Life Senior Care Solutions',
   OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
   URL: 'https://next-saas-starter-ashy.vercel.app/',
-  MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://gmail.us9.list-manage.com/subscribe/post?u=d0e71e77e22c543112dc699c9&amp;id=9aeb051fd4&amp;f_id=000214e1f0',
 };
